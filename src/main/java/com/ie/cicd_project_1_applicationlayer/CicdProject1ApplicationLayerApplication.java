839f386c3f587d121a9e01352f08fbc964bf8426
Lucas Jeanes <lucasjeanes@gmail.com>
2025-01-01 17:10:28 +0000

    Project setup

---
+++ src/main/java/com/ie/cicd_project_1_applicationlayer/CicdProject1ApplicationLayerApplication.java
@@ -0,0 +1,13 @@
+package com.ie.cicd_project_1_applicationlayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdProject1ApplicationLayerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdProject1ApplicationLayerApplication.class, args);
+    }
+
+}
