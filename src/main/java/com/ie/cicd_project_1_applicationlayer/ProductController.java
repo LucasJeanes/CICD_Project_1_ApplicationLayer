53746170a8db5f3410a79332cc9d8bd7f78d2e8c
Lucas Jeanes <lucasjeanes@gmail.com>
2025-01-02 20:49:25 +0000

    Implemented API connection functionality between ApplicationLayer and ServiceLayer. Ran into an issue with endpoints with openFeign not recognizing endpoints as it doesn't use the @RequestMapping when sending the request endpoint so I had to update the paths in the interface to reflect this. It's working now

--- src/main/java/com/ie/cicd_project_1_applicationlayer/ProductController.java
+++ src/main/java/com/ie/cicd_project_1_applicationlayer/ProductController.java
@@ -1,5 +1,7 @@
 package com.ie.cicd_project_1_applicationlayer;
 
+import com.ie.cicd_project_1_applicationlayer.repository.PortalServiceClient;
+import feign.FeignException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,41 +13,47 @@ import java.util.List;
 @RequestMapping("/products")
 public class ProductController {
 
-    private final ProductService productService;
+    private final PortalServiceClient portalServiceClient;
 
     @Autowired
-    public ProductController(ProductService productService) {
-        this.productService = productService;
+    public ProductController(PortalServiceClient portalServiceClient) {
+        this.portalServiceClient = portalServiceClient;
     }
 
     @PostMapping("/newProduct")
     public ResponseEntity<Product> createProduct(@RequestBody Product product) {
-        Product createdProduct = productService.createProduct(product);
+        Product createdProduct = portalServiceClient.createProduct(product);
         return new ResponseEntity<>(createdProduct, HttpStatus.CREATED);
     }
 
     @GetMapping
     public ResponseEntity<List<Product>> getAllProducts() {
-        List<Product> products = productService.getAllProducts();
+        List<Product> products = portalServiceClient.getAllProducts();
         return new ResponseEntity<>(products, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Product> getProductById(@PathVariable Long id) {
-        return productService.getProductById(id)
-                .map(product -> new ResponseEntity<>(product, HttpStatus.OK))
-                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+        try {
+            Product product = portalServiceClient.getProductById(id);
+            return new ResponseEntity<>(product, HttpStatus.OK);
+        } catch (FeignException.NotFound e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<Product> updateProduct(@PathVariable Long id, @RequestBody Product productDetails) {
-        Product updatedProduct = productService.updateProduct(id, productDetails);
-        return new ResponseEntity<>(updatedProduct, HttpStatus.OK);
+        try {
+            Product updatedProduct = portalServiceClient.updateProduct(id, productDetails);
+            return new ResponseEntity<>(updatedProduct, HttpStatus.OK);
+        } catch (FeignException.NotFound e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
-        productService.deleteProduct(id);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    public ResponseEntity<String> deleteProduct(@PathVariable Long id) {
+        return portalServiceClient.deleteProduct(id);
     }
 }
